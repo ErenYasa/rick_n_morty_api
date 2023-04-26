9a8a8ebe833ce33dd0c79b6c9ef257fb0733d6c2
Eren Yasa <eren.yasa@viravira.co>
2023-04-26 22:25:47 +0300

    add browsersync for mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import VitePluginBrowserSync from 'vite-plugin-browser-sync';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePluginBrowserSync({
+            bs: {
+                ui: {
+                    port: 8080,
+                },
+                port: 3033,
+                open: false,
+                notify: false,
+            },
+        }),
+    ],
     resolve: {
         alias: {
             src: `./src`,
