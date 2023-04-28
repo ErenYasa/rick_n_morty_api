e13d958e16543cc9313d88da81abb48f2fb2e8e5
Eren Yasa <eren.yasa@viravira.co>
2023-04-28 11:52:44 +0300

    update character card

--- src/components/Cards/CharacterCard.tsx
+++ src/components/Cards/CharacterCard.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { CharacterCardStyle } from '../../styles/components/CharacterCardStyle';
+import { Bottom, Card, Detail, Image, Name } from '../../styles/components/CharacterCardStyle';
 import { ICharacterCard } from './interfaces/characterCard.interface';
+import * as Icon from '../Icons/Icons';
 
 export function CharacterCard({ status, name, species }: ICharacterCard) {
-    return <CharacterCardStyle status={status} name={name} species={species} />;
+    return (
+        <Card>
+            <Image src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+            <Bottom>
+                <Name>{name}</Name>
+                <Detail status={status}>
+                    <Icon.Dot />
+                    {status} - {species}
+                </Detail>
+            </Bottom>
+        </Card>
+    );
 }
--- src/styles/components/CharacterCardStyle.tsx
+++ src/styles/components/CharacterCardStyle.tsx
@@ -1,16 +1,14 @@
 import styled from 'styled-components';
 import { Theme } from '../Theme';
-import * as Icon from '../../components/Icons/Icons';
-import { ICharacterCardStyle } from './interfaces/characterCardStyle.interface';
 
-const Card = styled.div`
+export const Card = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.9rem;
     cursor: pointer;
 `;
 
-const Image = styled.img`
+export const Image = styled.img`
     aspect-ratio: 1/1;
     width: 100%;
     height: auto;
@@ -18,15 +16,15 @@ const Image = styled.img`
     background-color: ${() => Theme.colors.placeholderGrey};
 `;
 
-const Bottom = styled.div``;
+export const Bottom = styled.div``;
 
-const Name = styled.div`
+export const Name = styled.div`
     margin-bottom: 0.6rem;
     font-size: ${() => Theme.typography.large};
     font-weight: 600;
 `;
 
-const Detail = styled.div<{ status: string }>`
+export const Detail = styled.div<{ status: string }>`
     display: flex;
     align-items: center;
     gap: 0.6rem;
@@ -37,24 +35,3 @@ const Detail = styled.div<{ status: string }>`
         color: ${({ status }) => (status === 'alive' ? '#98CD4D' : status === 'dead' ? '#B90000' : '#B8B8B8')};
     }
 `;
-
-const DetailTop = styled.div``;
-
-const DetailBottom = styled.div``;
-
-export const CharacterCardStyle = ({ status, name, species }: ICharacterCardStyle) => (
-    <Card>
-        <Image src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-        <Bottom>
-            <Name>{name}</Name>
-            <Detail status={status}>
-                <DetailTop>
-                    {' '}
-                    <Icon.Dot />
-                    {status} - {species}
-                </DetailTop>
-                <DetailBottom></DetailBottom>
-            </Detail>
-        </Bottom>
-    </Card>
-);
