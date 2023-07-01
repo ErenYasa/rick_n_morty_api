4662eacdd2f3f941ee058985d08c13302a14f64f
Eren Yasa <eren.yasa@viravira.co>
2023-07-01 13:59:22 +0300

    module import bug fix for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FooterStyle } from '../../styles/components/Footer/footerStyle';
+import { FooterStyle } from '../../styles/components/Footer/FooterStyle';
 
 export default function Footer({ text }: { text: string }) {
     return <FooterStyle>{text}</FooterStyle>;
